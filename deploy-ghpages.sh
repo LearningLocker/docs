bbfcaf7e7790949898dcdeb768f2905a16d8bb0a
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-13 13:25:44 +0100

    Runs the travis build if on the master branch

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -11,22 +11,15 @@ gem install jekyll
 jekyll build --destination out
 echo "${DELIM}Finished build${DELIM}"
 
-# Branch
-BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-MASTER="master"
-echo "${DELIM}Comparing ${MASTER} to ${BRANCH}${DELIM}"
-
 # Deploy if on master branch
-if [ "${MASTER}" = "${BRANCH}" ]; then
-	echo "${DELIM}Starting deployment${DELIM}"
-	cd out
-	git init
-	git config user.name "Travis-CI"
-	git config user.email "travis@nodemeatspace.com"
-	cp ../CNAME ./CNAME
-	cp ../countryiso.js ./countryiso.js
-	git add .
-	git commit -m "Deployed to Github Pages"
-	git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
-	echo "${DELIM}Finished deployment${DELIM}"
-fi
+echo "${DELIM}Starting deployment${DELIM}"
+cd out
+git init
+git config user.name "Travis-CI"
+git config user.email "travis@nodemeatspace.com"
+cp ../CNAME ./CNAME
+cp ../countryiso.js ./countryiso.js
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+echo "${DELIM}Finished deployment${DELIM}"
