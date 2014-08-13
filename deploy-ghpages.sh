b2f6c736ba5ae2540b60cf1e5731f4c8fc37a471
Ryan Smith <0ryansmith1994@gmail.com>
2014-08-13 12:45:03 +0100

    Improves build file for debugging.

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env sh
-rm -rf out || exit 0;
-mkdir out; 
-gem install jekyll;
-jekyll build --destination out;
-BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
-MASTER="master";
 
+# Colours
+START='\e['
+CYAN='${START}0;36m'
+NC='${START}0m'
+
+# Build
+echo "${CYAN}Starting build${NC}"
+rm -rf out || exit 0
+mkdir out
+gem install jekyll
+jekyll build --destination out
+echo "${CYAN}Finished build${NC}"
+
+# Branch
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+MASTER="master"
+
+# Deploy if on master branch
 if [ "${MASTER}" == "${BRANCH}" ]; then
-	( echo "Deploying to Github Pages"
+	( echo "${CYAN}Starting deployment${NC}"
 	 cd out
 	 git init
 	 git config user.name "Travis-CI"
@@ -17,5 +29,6 @@ if [ "${MASTER}" == "${BRANCH}" ]; then
 	 git add .
 	 git commit -m "Deployed to Github Pages"
 	 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+	 echo "${CYAN}Finished deployment${NC}"
 	)
 fi
